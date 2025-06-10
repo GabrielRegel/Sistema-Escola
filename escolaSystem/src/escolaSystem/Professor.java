6b838a77faece7a6803fc31d433f30f286b55b93
Gabriel Regel <161236355+GabrielRegel@users.noreply.github.com>
2025-06-10 09:27:38 -0300

    Classe Professor finalizada

--- escolaSystem/src/escolaSystem/Professor.java
+++ escolaSystem/src/escolaSystem/Professor.java
@@ -1,5 +1,37 @@
 package escolaSystem;
 
-public class Professor {
+public class Professor extends Pessoa {
+	private String diciplina;
+	private String formacao;
+	
+	public Professor(String nome, int idade, String email, String cpf, String diciplina, String formacao) {
+		super(nome, idade, email, cpf);
+		this.diciplina = diciplina;
+		this.formacao = formacao;
+	}
 
+	public String getDiciplina() {
+		return diciplina;
+	}
+
+	public void setDiciplina(String diciplina) {
+		this.diciplina = diciplina;
+	}
+
+	public String getFormacao() {
+		return formacao;
+	}
+
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	
+	public void darAula() {
+	    System.out.println(getNome() + " formado em " + formacao + " está dando aula de " + diciplina + ".");
+	}
+	
+	@Override
+	public void apresentar() {
+		System.out.println("Sou professor formado em " + formacao + " e dou aula de " + diciplina + ".");
+	}
 }
